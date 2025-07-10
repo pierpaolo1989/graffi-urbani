fc1979972398ac65aa5559e49825a4d95f89decb
pierpaolo1989 <pierpaolo.pdd@gmail.com>
2025-07-10 19:39:55 +0200

    Added hashrouter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, HashRouter as Router, Routes } from 'react-router-dom';
 import "./App.css";
 import Artists from "./pages/ArtistsPage.tsx";
+import CuriosityPage from "./pages/CuriosityPage.tsx";
 import MainPage from "./pages/MainPage.tsx";
 import NoPage from "./pages/NoPage.tsx";
-import CuriosityPage from "./pages/CuriosityPage.tsx";
 
 const App = () => {
   return (
-
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<MainPage />} />
-      <Route index element={<MainPage />} />
-      <Route path="/artists" element={<Artists />} />
-      <Route path="*" element={<NoPage />} />
-      <Route path="/curiosity" element={<CuriosityPage/>} />
-    </Routes>
-  </BrowserRouter>
+<Router>
+  <Routes>
+    <Route path="/" element={<MainPage />} />
+    <Route index element={<MainPage />} />
+    <Route path="/artists" element={<Artists />} />
+    <Route path="/curiosity" element={<CuriosityPage />} />
+    <Route path="*" element={<NoPage />} />
+  </Routes>
+</Router>
   );
 };
 
